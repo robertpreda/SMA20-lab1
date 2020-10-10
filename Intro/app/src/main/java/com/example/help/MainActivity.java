cf1ebb7c3252e36270cc63dc0cc5a9e23154f95b
Robert Preda <predarobert7@gmail.com>
2020-10-11 00:00:56 +0300

    Solved crashing issue

--- Intro/app/src/main/java/com/example/help/MainActivity.java
+++ Intro/app/src/main/java/com/example/help/MainActivity.java
@@ -13,14 +13,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText eName = (EditText)findViewById(R.id.eName);
-    TextView tName = (TextView)findViewById(R.id.tName);
-    Button bClick = (Button)findViewById(R.id.bClick);
+    private EditText eName;
+    private TextView tName;
+    private Button bClick;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.eName = (EditText)findViewById(R.id.eName);
+        this.bClick = (Button)findViewById(R.id.bClick);
     }
 
     public void clicked(View view)
@@ -51,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),"You're ugly, " + eName.getText().toString(),Toast.LENGTH_LONG).show();
                     }
                 }
-                );
+                )
+        .show();
 
 
     }

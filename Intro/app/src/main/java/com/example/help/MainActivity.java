bf22e186e456dc3c89f2da00daba86c44a19fafc
Robert Preda <predarobert7@gmail.com>
2020-10-10 20:08:11 +0300

    Small changes, still crashes

--- Intro/app/src/main/java/com/example/help/MainActivity.java
+++ Intro/app/src/main/java/com/example/help/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.help;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +27,33 @@ public class MainActivity extends AppCompatActivity {
     {
         switch(view.getId()){
             case R.id.bClick:
-                String name = eName.getText().toString();
-                tName.setText("Hi, " + name);
+                alert();
                 break;
             default: break;
         }
+    }
+
+    public void alert(){
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("How do you want to be greeted?");
+        dialog.setTitle("Greeting dialog");
+        dialog.setPositiveButton("Politely",
+        new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,
+                                int which) {
+                Toast.makeText(getApplicationContext(),"Hello there, " + eName.getText().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        dialog.setNegativeButton("Roast me",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Toast.makeText(getApplicationContext(),"You're ugly, " + eName.getText().toString(),Toast.LENGTH_LONG).show();
+                    }
+                }
+                );
+
+
     }
 
 
